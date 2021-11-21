231cdb220f2fac8ed5e3104aaefbdf298a1ace1a
MarilinKatherin <A1110060@idat.edu.pe>
2021-11-21 18:50:08 -0500

    modificaciones para menu unico con jquery

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
-//document.getElementById("icon-menu").addEventListener("click", mostrar_menu);
 
-function mostrar_menu(){
+function mostrar_menu() {
 
     document.getElementById("move-content").classList.toggle('move-container-all');
     document.getElementById("show-menu").classList.toggle('show-lateral');
@@ -8,73 +7,31 @@ function mostrar_menu(){
 
 
 
-
-
-
-
-
-                            //Buscador de contenido
-
-
-//Ejecutando funciones
-// document.getElementById("icon-search").addEventListener("click", mostrar_buscador);
-// document.getElementById("cover-ctn-search").addEventListener("click", ocultar_buscador);
-
-//Declarando variables
-bars_search =       document.getElementById("ctn-bars-search");
-cover_ctn_search =  document.getElementById("cover-ctn-search");
-inputSearch =       document.getElementById("inputSearch");
-box_search =        document.getElementById("box-search");
-
-
-//Funcion para mostrar el buscador
-function mostrar_buscador(){
-
-    bars_search.style.top = "80px";
-    cover_ctn_search.style.display = "block";
-    inputSearch.focus();
-
-    if (inputSearch.value === ""){
-        box_search.style.display = "none";
-    }
-
-}
-
-//Funcion para ocultar el buscador
-function ocultar_buscador(){
-
-    bars_search.style.top = "-10px";
-    cover_ctn_search.style.display = "none";
-    inputSearch.value = "";
-    box_search.style.display = "none";
-
-}
-
-
 //Creando filtrado de busqueda
+function CrearBuscador() {
 
 
-const field = document.getElementById('inputSearch');
+    const field = document.getElementById('inputSearch');
     const ac = new Autocomplete(field, {
         maximumItems: 5,
         treshold: 1,
-        onSelectItem: ({label, value}) => {
-         switch (value) {
-            case "1":
-                location.href = "cursos.html";
-                break;
-            case "2":
-                location.href = "nosotros.html";
-                break;				
-            case "3":
-                location.href = "Contacto.html";
-                break;
-			case "4":
-               
-                break;
-            default:
-                break;
-        }
+        onSelectItem: ({ label, value }) => {
+            switch (value) {
+                case "1":
+                    location.href = "cursos.html";
+                    break;
+                case "2":
+                    location.href = "nosotros.html";
+                    break;
+                case "3":
+                    location.href = "Contacto.html";
+                    break;
+                case "4":
+
+                    break;
+                default:
+                    break;
+            }
         }
     });
 
@@ -90,8 +47,16 @@ const field = document.getElementById('inputSearch');
             "label": "Contacto",
             "value": "3"
         },
-		{
+        {
             "label": "Ayuda",
             "value": "4"
         }
-		]);
+    ]);
+
+}
+
+//Insertar Menu
+
+$("#menu").load("menu.html", function (e) {
+    CrearBuscador();
+});
